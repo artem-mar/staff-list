1ed51ab09d66c944b6ffdac579fe206956e662ed
Artem M <artemmarin91@mail.ru>
2022-12-23 04:29:24 +0300

    upd routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 const apiPath = 'https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users';
 
 const routes = {
-  usersApiPath: () => [apiPath, 'users'].join('/'),
-  usersByDepartmentApiPath: (department) => [apiPath, `?__example=${department}`].join('/'),
+  usersApiPath: () => apiPath,
+  usersByDepartmentApiPath: (department) => `${apiPath}?__example=${department}`,
   mainPath: () => '/',
   userInfoPath: () => '/user-info',
 };
