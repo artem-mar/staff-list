8f178b6dec82bebb2c9bb71006e83006f1e4ead6
Artem M <artemmarin91@mail.ru>
2022-12-24 05:04:08 +0300

    added critical error element

---
+++ src/components/CriticalError.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import nloImg from '../assets/nlo.svg';
+
+const CriticalError = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="d-flex justify-content-center pt-5">
+      <div className="text-center mt-5">
+        <img src={nloImg} alt="magnifying-glass" />
+        <p className="mt-3 fw-semibold fs-5">{t('error.everythingIsBroken')}</p>
+        <p><small className="text-muted fs-5">{t('error.tryToFix')}</small></p>
+        <p><a href="/" className="text-primary fs-5">{t('error.tryAgain')}</a></p>
+      </div>
+    </div>
+  );
+};
+
+export default CriticalError;
