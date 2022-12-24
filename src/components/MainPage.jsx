b2085435fabbffcded78ebbdbcb148d2f5b6a135
Artem M <artemmarin91@mail.ru>
2022-12-24 05:06:06 +0300

    deleted StaffContainer

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import StaffContainer from './StaffContainer.jsx';
+import UsersList from './UsersList.jsx';
 import TopBar from './TopBar.jsx';
 import ModalWindow from './Modal.jsx';
 
-const MainPage = () => {
-  console.log();
-  return (
-    <div className="vh-100 d-flex flex-column ps-3 pe-3 pt-3">
-      <TopBar />
-      <StaffContainer />
-      <ModalWindow />
-    </div>
-  );
-};
+const MainPage = () => (
+  <div className="d-flex flex-column">
+    <TopBar />
+    <UsersList />
+    <ModalWindow />
+  </div>
+);
 
 export default MainPage;
