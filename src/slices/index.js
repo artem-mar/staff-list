48806fa65c55a18f7a283aca888948995be26f57
Artem M <artemmarin91@mail.ru>
2022-12-28 03:24:01 +0300

    added fetchUserById thunk

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import usersReducer, { actions as userActions } from './usersSlice.js';
+import usersReducer, { actions as userActions, fetchUserById } from './usersSlice.js';
 import modalReducer, { actions as modalActions } from './modalSlice.js';
 
 export const actions = {
   ...userActions,
   ...modalActions,
+  fetchUserById,
 };
 
 export default configureStore({
--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -1,8 +1,28 @@
 /* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import routes from '../routes.js';
+
+export const fetchUserById = createAsyncThunk(
+  'users/fetchUserById',
+  async (id) => {
+    const { data } = await axios.get(routes.usersByDepartmentApiPath());
+    return data.items.find((user) => user.id === id);
+  },
+  {
+    condition: (id, { getState }) => {
+      const { users } = getState();
+      if (users.users.find((user) => user.id === id)) {
+        return false;
+      }
+      return true;
+    },
+  },
+);
 
 const initialState = {
   users: [],
+  fetching: 'idle', // pending | fulfilled | rejected
   department: 'all',
   searchString: '',
   sortingRule: 'byFirstName',
@@ -24,6 +44,19 @@ const usersSlice = createSlice({
     setSortingRule: (state, { payload }) => {
       state.sortingRule = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserById.fulfilled, (state, { payload }) => {
+        state.users.push(payload);
+        state.fetching = 'fulfilled';
+      })
+      .addCase(fetchUserById.pending, (state) => {
+        state.fetching = 'pending';
+      })
+      .addCase(fetchUserById.rejected, (state) => {
+        state.fetching = 'rejected';
+      });
   },
 });
 
